17b08cfea7c825bf03258e84dc40dcfe14196940
Maxim Vetrenko <maxim@vetrenko.com>
2019-10-28 11:46:20 +0100

    Removed counter reducers and sagas

--- src/logic/root-saga.js
+++ src/logic/root-saga.js
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
-import { counterSaga } from './counter/sagas/counter';
 import { restaurantsListSaga } from './restaurants-list/sagas/restaurant-list';
 
 export function* rootSaga() {
-  yield all([counterSaga(), restaurantsListSaga()]);
+  yield all([restaurantsListSaga()]);
 }
