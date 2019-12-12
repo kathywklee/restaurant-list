0d62b6e2ba3eca3c553a030320491b2f37d25c1f
Maxim Vetrenko <maxim.vetrenko@takeaway.com>
2019-12-12 11:50:46 +0100

    Layout for sortings and Modal window

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -41,7 +41,7 @@ export default class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
-        <body onTouchStart="">
+        <body onTouchStart="" id="root">
           <Main />
           <NextScript />
         </body>
